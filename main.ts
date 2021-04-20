7d2456861daba9c22bb6479f5ef272dbfa464cc9
Joaquimmajo <82875861+Joaquimmajo@users.noreply.github.com>
2021-04-20 12:47:42 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,22 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+let x = -1
+let y = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        x += 1
+        led.plot(x, y)
+        basic.pause(100)
+        if (x == 6) {
+            x = -1
+            y += 1
+        }
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        led.unplot(x, y)
+        x += -1
+        basic.pause(100)
+        if (x == -1) {
+            x = 5
+            y += -1
+        }
+    }
 })
